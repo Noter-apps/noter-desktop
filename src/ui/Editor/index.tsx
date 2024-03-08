57b2a1037c2e284dd82ba6403c146374bfe9b525
OndrejHana <ondrahana001@gmail.com>
2024-03-08 16:20:19 +0100

    limited text window width for readability

--- src/ui/Editor/index.tsx
+++ src/ui/Editor/index.tsx
@@ -1,6 +1,6 @@
 import {
-    InitialConfigType,
-    LexicalComposer,
+  InitialConfigType,
+  LexicalComposer,
 } from "@lexical/react/LexicalComposer";
 import { $convertFromMarkdownString } from "@lexical/markdown";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
@@ -13,61 +13,103 @@ import { ListNode, ListItemNode } from "@lexical/list";
 import { CodeNode, CodeHighlightNode } from "@lexical/code";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import { EditorState } from "lexical";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import putFile from "@/types/commands/putFile";
 
+function AutoSavePlugin({
+  onSave,
+}: {
+  onSave: (editorState: EditorState) => void;
+}) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-function AutoSavePlugin({ onSave }: { onSave: (editorState: EditorState) => void }) {
-    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-
-    function onChange(editorState: EditorState) {
-        if (timeoutRef.current) {
-            clearInterval(timeoutRef.current);
-        }
-
-        timeoutRef.current = setTimeout(() => {
-            onSave(editorState);
-        }, 1000);
+  function onChange(editorState: EditorState) {
+    if (timeoutRef.current) {
+      clearInterval(timeoutRef.current);
     }
 
-    return <OnChangePlugin ignoreSelectionChange={true} onChange={onChange} />;
+    timeoutRef.current = setTimeout(() => {
+      onSave(editorState);
+    }, 1000);
+  }
+
+  return <OnChangePlugin ignoreSelectionChange={true} onChange={onChange} />;
 }
 
-export default function Editor({ note, onSave }: { note: Note, onSave: (editorState:EditorState) => void }) {
-    const initialConfig = {
-        namespace: "editor",
-        editable: true,
-        onError(error) {
-            console.log("Error", error);
-        },
-        editorState: (editorState) => {
-            $convertFromMarkdownString(note.body);
-        },
-        theme,
-        nodes: [
-            HeadingNode,
-            ListNode,
-            ListItemNode,
-            QuoteNode,
-            CodeNode,
-            CodeHighlightNode,
-            AutoLinkNode,
-            LinkNode,
-        ],
-    } satisfies InitialConfigType;
+export default function Editor({
+  note,
+  onSave,
+}: {
+  note: Note;
+  onSave: (editorState: EditorState) => void;
+}) {
+  const initialConfig = {
+    namespace: "editor",
+    editable: true,
+    onError(error) {
+      console.log("Error", error);
+    },
+    editorState: (editorState) => {
+      $convertFromMarkdownString(note.body);
+    },
+    theme,
+    nodes: [
+      HeadingNode,
+      ListNode,
+      ListItemNode,
+      QuoteNode,
+      CodeNode,
+      CodeHighlightNode,
+      AutoLinkNode,
+      LinkNode,
+    ],
+  } satisfies InitialConfigType;
 
-    return (
-        <LexicalComposer initialConfig={initialConfig}>
-            <RichTextPlugin
-                placeholder={<div>"Start typing..."</div>}
-                contentEditable={
-                    <ContentEditable className="border-none outline-none block p-2" />
-                }
-                ErrorBoundary={LexicalErrorBoundary}
-            ></RichTextPlugin>
-            <AutoSavePlugin onSave={onSave}/>
-        </LexicalComposer>
-    );
+  const [floatingAnchorElem, setFloatingAnchorElem] =
+    useState<HTMLDivElement | null>(null);
+  const [isSmallWidthViewport, setIsSmallWidthViewport] =
+    useState<boolean>(false);
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem);
+    }
+  };
+
+  useEffect(() => {
+    const updateViewPortWidth = () => {
+      const isNextSmallWidthViewport = window.matchMedia(
+        "(max-width: 1025px)",
+      ).matches;
+
+      if (isNextSmallWidthViewport !== isSmallWidthViewport) {
+        setIsSmallWidthViewport(isNextSmallWidthViewport);
+      }
+    };
+    updateViewPortWidth();
+    window.addEventListener("resize", updateViewPortWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateViewPortWidth);
+    };
+  }, [isSmallWidthViewport]);
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        placeholder={<div>"Start typing..."</div>}
+        contentEditable={
+          <div
+            className={`w-full h-full mx-auto ${isSmallWidthViewport ? "w-full" : "lg:max-w-2xl"}`}
+          >
+            <div ref={onRef} className="w-full h-full">
+              <ContentEditable className="border-none outline-none p-2 w-full h-full" />
+            </div>
+          </div>
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      ></RichTextPlugin>
+      <AutoSavePlugin onSave={onSave} />
+    </LexicalComposer>
+  );
 }
